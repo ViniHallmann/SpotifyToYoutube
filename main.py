68eb72c7d333ae9f86d8b810043db13a11f86bfb
Vinícius <viniciushallmann2015@gmail.com>
2024-07-15 16:51:04 -0300

    código refatorado

--- main.py
+++ main.py
@@ -15,46 +15,42 @@ playlist_info = []
 #LOAD DAS VARIAVEIS DO SPOTIFY NO .ENV
 def get_spotify_variables_env():
     load_dotenv()
-    return os.getenv("SPOTIFY_CLIENT_ID"), os.getenv("SPOTIFY_CLIENT_SECRET"), os.getenv("SPOTIFY_PLAYLIST_TRACKS")
+    return os.getenv("SPOTIFY_CLIENT_ID"), os.getenv("SPOTIFY_CLIENT_SECRET")
 
 #LOAD DAS VARIAVEIS DO YOUTUBE NO .ENV
 def get_youtube_variables_env():
     load_dotenv()
-    return os.getenv("YOUTUBE_CLIENT_ID"), os.getenv("YOUTUBE_CLIENT_SECRET")
+    return os.getenv("YOUTUBE_CLIENT_ID"), os.getenv("YOUTUBE_CLIENT_SECRET"), os.getenv("YOUTUBE_TRACKS_LINKS")
 
-#PEGANDO AS VARIAVEIS DO SPOTIFY E DO YOUTUBE
-spotify_client_id, spotify_client_secret, spotify_saved_playlist = get_spotify_variables_env()
-youtube_client_id, youtube_client_secret                         = get_youtube_variables_env()
-
-# autenticando com o client_secret.json
+#Autenticando com o client_secret.json
 SCOPES = ['https://www.googleapis.com/auth/youtube']
 CLIENT_SECRETS_FILE = "client_secret.json"
 
 #FUNÇÕES LADO YOUTUBE
-"""
-    FUNÇÃO PARA AUTENTICAR NO YOUTUBE
-    AUTOR = Christian (Alemão) 
-"""
 def get_authenticated_service() -> str:
+    """
+        FUNÇÃO PARA AUTENTICAR NO YOUTUBE
+        AUTOR = Christian (Alemão) 
+    """
     flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
     credentials = flow.run_local_server(port=8080, prompt='consent', authorization_prompt_message='')
     return credentials
 
-"""
-    FUNÇÃO PARA BUSCAR MUSICA NO YOUTUBE -> RETORNA URL
-    AUTOR = Christian (Alemão)
-"""
 def search_youtube(item) -> str:
+    """
+        FUNÇÃO PARA BUSCAR MUSICA NO YOUTUBE -> RETORNA URL
+        AUTOR = Christian (Alemão)
+    """
     videosSearch = VideosSearch(item, limit=1)
     result = videosSearch.result()
     video_url = result['result'][0]['link']
     return video_url
 
-"""
-    FUNÇÃO PARA CRIAR PLAYLIST NO YOUTUBE CONFORME TITULO E DESCRIÇÃO NOS PARÂMETROS DA FUNÇÃO
-    AUTOR = Christian (Alemão)
-"""
 def create_playlist(youtube, title, description) -> str:
+    """
+        FUNÇÃO PARA CRIAR PLAYLIST NO YOUTUBE CONFORME TITULO E DESCRIÇÃO NOS PARÂMETROS DA FUNÇÃO
+        AUTOR = Christian (Alemão)
+    """
     request = youtube.playlists().insert(
         part="snippet,status",
         body={
@@ -69,12 +65,12 @@ def create_playlist(youtube, title, description) -> str:
     )
     response = request.execute()
     return response['id']
-
-"""
-    FUNÇÃO PARA ADICIONAR MUSICA NA PLAYLIST DO YOUTUBE A PARTIR DO ID DA MUSICA E DA PLAYLIST
-    AUTOR = Christian (Alemão)
-"""    
+ 
 def add_song_youtube(youtube, playlist_id, video_id) -> dict:  
+    """
+        FUNÇÃO PARA ADICIONAR MUSICA NA PLAYLIST DO YOUTUBE A PARTIR DO ID DA MUSICA E DA PLAYLIST
+        AUTOR = Christian (Alemão)
+    """   
     request = youtube.playlistItems().insert(
         part="snippet",
         body={
@@ -90,13 +86,57 @@ def add_song_youtube(youtube, playlist_id, video_id) -> dict:
     response = request.execute()
     return response
 
-#========================================================================================================================================================#
-# FUNÇÕES LADO SPOTIFY
-"""
-    FUNÇÃO PARA PEGAR O TOKEN DE AUTOENTICAÇÃO DO SPOTIFY
-    AUTOR = Vine
-"""
+def search_youtube_link(playlist_info) -> list:
+    """
+        FUNÇÃO PARA BUSCAR MUSICA NO YOUTUBE -> RETORNA PLAYLIST ATUALIZADA COM URL
+        AUTOR = Vine
+    """
+    for item in playlist_info:
+        artist_title_yt_search = (f"{item['title']} - {item['artist']}") 
+        ytLink = search_youtube(artist_title_yt_search) 
+        print(ytLink)
+        if ytLink:
+            item['ytLink'] = ytLink
+        else:
+            print(f"Não foi possível encontrar um link para '{item['artist']} - {item['title']}' no YouTube.")
+
+    return playlist_info
+
+def send_to_youtube(playlist_info) -> None:
+    """
+        FUNÇÃO PARA ENVIAR AS MÚSICAS DA PLAYLIST DO SPOTIFY PARA O YOUTUBE
+        AUTOR = Vine e Christian
+    """
+    for item in playlist_info:
+        video_id = item['ytLink'].split("v=")[1].split("&")[0]     # Pega o ID da música com base no link 
+        add_song_youtube(youtube, playlist_youtube_id, video_id)   # Adiciona a música a playlist definida na chamada da função
+    return None
+
+def send_links_to_youtube(env_links) -> None:
+    """
+        FUNÇÃO PARA ENVIAR OS LINKS DAS MÚSICAS DA PLAYLIST DO SPOTIFY PARA O YOUTUBE
+        AUTOR = Vine e Christian
+    """
+    cleaned_links = env_links.strip('[]')
+    links_list = cleaned_links.split(", ")
+    
+    # Remova as aspas do primeiro e do último link, se necessário
+    if links_list[0].startswith('"'):
+        links_list[0] = links_list[0][1:]
+    if links_list[-1].endswith('"'):
+        links_list[-1] = links_list[-1][:-1]
+    
+    for link in links_list:
+        link = link.strip('"')
+        video_id = link.split("v=")[1].split("&")[0]
+        add_song_youtube(youtube, playlist_youtube_id, video_id)
+     
+#FUNÇÕES LADO SPOTIFY
 def get_token() -> str:
+    """
+        FUNÇÃO PARA PEGAR O TOKEN DE AUTOENTICAÇÃO DO SPOTIFY
+        AUTOR = Vine
+    """
     auth_string = f"{spotify_client_id}:{spotify_client_secret}"
     auth_bytes = auth_string.encode('utf-8')
     auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
@@ -113,19 +153,18 @@ def get_token() -> str:
     token = json_result["access_token"]
     return token
 
-"""
-    FUNÇÃO PARA PEGAR HEADER DE AUTENTICAÇÃO DO SPOTIFY
-    AUTOR = Vine
-"""
 def get_auth_header(token) -> dict:
-
+    """
+        FUNÇÃO PARA PEGAR HEADER DE AUTENTICAÇÃO DO SPOTIFY
+        AUTOR = Vine
+    """
     return {"Authorization": f"Bearer {token}"}
 
-"""
-    FUNÇÃO PARA PEGAR AS MÚSICAS DA PLAYLIST DO SPOTIFY
-    AUTOR = Vine
-"""
 def get_playlist_tracks(token, playlist_id) -> list:
+    """
+        FUNÇÃO PARA PEGAR AS MÚSICAS DA PLAYLIST DO SPOTIFY
+        AUTOR = Vine
+    """
     url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
     headers = get_auth_header(token)
     result = get(url, headers=headers)
@@ -134,21 +173,18 @@ def get_playlist_tracks(token, playlist_id) -> list:
     extract_track_info(json_result) 
 
     while next_url != None:
+        if next_url == None: break
         result = get(next_url, headers=headers)
         json_result = json.loads(result.content)
-        #print(json_result["items"][0]["track"]["name"])
         next_url = json_result["next"]
         extract_track_info(json_result) 
-        #return extract_track_info(json_result) 
 
-"""
-    FUNÇÃO PARA EXTRAIR AS INFORMAÇÕES DAS MÚSICAS DA PLAYLIST DO SPOTIFY
-    AUTOR = Vine e Christian
-"""
+    return playlist_info
 
-def extract_track_info(playlist_json) -> list:
-    """with open("playlist.json", "w") as file:
-        json.dump(playlist_json, file, indent=4)
+def extract_track_info(playlist_json) -> None:
+    """
+        FUNÇÃO PARA EXTRAIR AS INFORMAÇÕES DAS MÚSICAS DA PLAYLIST DO SPOTIFY
+        AUTOR = Vine e Christian
     """
     if "tracks" in playlist_json: 
         for item in playlist_json["tracks"]["items"]:
@@ -157,10 +193,6 @@ def extract_track_info(playlist_json) -> list:
             else: 
                 artist = item["track"]["artists"][0]["name"]
                 title = item["track"]["name"]
-                print(item["track"]["name"])
-            #print(title)
-        #artist = item["track"]["artists"][0]["name"]
-        #title = item["track"]["name"]
                 playlist_info.append({"artist": artist, "title": title})
     else: 
         for item in playlist_json["items"]:
@@ -169,72 +201,41 @@ def extract_track_info(playlist_json) -> list:
             else: 
                 artist = item["track"]["artists"][0]["name"]
                 title = item["track"]["name"]
-                print(item["track"]["name"])
                 playlist_info.append({"artist": artist, "title": title})
 
-    """for item in playlist_json["tracks"]["items"]:
-        artist = item["track"]["artists"][0]["name"]
-        title = item["track"]["name"]
-        playlist_info.append({"artist": artist, "title": title})\
-    """
-        #artist_title_yt_search = (f"{title} - {artist}") # junta título da música e artista pra busca mais precisa no yt
-        #ytLink = search_youtube(artist_title_yt_search) # procura a musica e salva o link 
-        
-        #if ytLink:
-          #  video_id = ytLink.split("v=")[1].split("&")[0] # pega o id da musica com base no link 
-          #  add_song_youtube(youtube, playlist_youtube_id, video_id) # adiciona a música a playlist definida na chamada da func
-          #  playlist_info.append({"artist": artist, "title": title, "ytLink" : ytLink})
-        
-      #  else:
-          #  print(f"Não foi possível encontrar um link para '{artist} - {title}' no YouTube.") # caso nao ache a musica 
-
-    #return playlist_info
-
-"""
-    FUNÇÃO PARA BUSCAR MUSICA NO YOUTUBE -> RETORNA PLAYLIST ATUALIZADA COM URL
-    AUTOR = Vine
-"""
-def search_youtube_link(playlist_info) -> list:
-
-    for item in playlist_info:
-        artist_title_yt_search = (f"{item['title']} - {item['artist']}") # junta título da música e artista pra busca mais precisa no yt
-        ytLink = search_youtube(artist_title_yt_search) # procura a musica e salva o link 
-        
-        if ytLink:
-            item['ytLink'] = ytLink
-        else:
-            print(f"Não foi possível encontrar um link para '{item['artist']} - {item['title']}' no YouTube.") # caso nao ache a musica 
-
-    return playlist_info
-
-"""
-    FUNÇÃO PARA ENVIAR AS MÚSICAS DA PLAYLIST DO SPOTIFY PARA O YOUTUBE
-    AUTOR = Vine e Christian
-"""
-def send_to_youtube(playlist_info) -> None:
-    for item in playlist_info:
-        video_id = item['ytLink'].split("v=")[1].split("&")[0]     # Pega o ID da música com base no link 
-        add_song_youtube(youtube, playlist_youtube_id, video_id)   # Adiciona a música a playlist definida na chamada da função
-    return None
-
-"""
-    FUNÇÃO PARA SALVAR AS MÚSICAS DA PLAYLIST DO SPOTIFY NO .ENV
-    AUTOR = Vine
-"""
 def save_to_env(playlist, id = None) -> None:
-
+    """
+        FUNÇÃO PARA SALVAR AS MÚSICAS DA PLAYLIST DO SPOTIFY NO .ENV
+        AUTOR = Vine
+    """
     playlist_tracks_str = json.dumps(playlist, ensure_ascii=False)
     env_file = os.path.join(os.path.dirname(__file__), '.env')
-    set_key(env_file, 'PLAYLIST_TRACKS', playlist_tracks_str)
-    #set_key(env_file, "PLAYLIST_TRACKS", f"{id}=!=!={playlist_tracks_str}")
+    set_key(env_file, 'SPOTIFY_PLAYLIST_TRACKS', playlist_tracks_str)
     return None
 
-"""
-    FUNÇÃO PARA VERIFICAR SE HÁ NOVAS MÚSICAS NA PLAYLIST DO SPOTIFY
-    AUTOR = Vine e Christian
-"""
-def has_new_music(playlist, id) -> None:
+def extract_links(playlist) -> list:
+    """
+    FUNÇÃO PARA EXTRAIR OS LINKS DAS MÚSICAS DA PLAYLIST
+    AUTOR = Vine
+    """
+    links = [item['ytLink'] for item in playlist if 'ytLink' in item]
+    return links
 
+def save_links_to_env(links) -> None:
+    """
+    FUNÇÃO PARA SALVAR OS LINKS DAS MÚSICAS EM UM NOVO .ENV
+    AUTOR = Vine
+    """
+    links_str = json.dumps(links, ensure_ascii=False)
+    env_file = os.path.join(os.path.dirname(__file__), '.env')
+    set_key(env_file, 'SPOTIFY_TRACKS_LINKS', links_str)
+    return None
+
+def has_new_music(playlist, id) -> None:
+    """
+        FUNÇÃO PARA VERIFICAR SE HÁ NOVAS MÚSICAS NA PLAYLIST DO SPOTIFY
+        AUTOR = Vine e Christian
+    """
     saved_playlist = os.getenv("PLAYLIST_TRACKS")
     playlist_dumped = json.dumps(playlist, ensure_ascii=False)
 
@@ -252,19 +253,28 @@ def has_new_music(playlist, id) -> None:
 #========================================================================================================================================================#
 #YOUTUBE CONFIGS
 """
-credentials = get_authenticated_service()                                   # autenticação no youtube
-youtube = build('youtube', 'v3', credentials=credentials)                   # criação do objeto youtube
-playlist_title = "Mandelão do FDS"                                          # titulo da playlist
-description = "Só as boas"                                                  # descrição da playlist
-playlist_youtube_id = create_playlist(youtube, playlist_title, description) # cria uma playlist com base nas config 
+credentials = get_authenticated_service()                                    # autenticação no youtube
+youtube = build('youtube', 'v3', credentials=credentials)                    # criação do objeto youtube
+#playlist_title = "Mandelão do FDS"                                          # titulo da playlist
+#description = "Só as boas"                                                  # descrição da playlist
+#playlist_youtube_id = create_playlist(youtube, playlist_title, description) # cria uma playlist com base nas config 
 """
+playlist_youtube_id = "PLJx7IIF4C47C-p_1epjTOwMm4jlmRzrEu"
+
+spotify_client_id, spotify_client_secret = get_spotify_variables_env()
+youtube_client_id, youtube_client_secret, youtube_links = get_youtube_variables_env()
 
 #SPOTIFY CONFIGS
-#3aJsKbVHL3U3ZngKqoVUZo?si=aa77820410b34402
+"""
 playlist_id = "3aJsKbVHL3U3ZngKqoVUZo?si=aa77820410b34402"                            # id da playlist do spotify
-#playlist_id = "2A49ff4cDR5xLqzg8L511Q?si=182bc5f9450e412a"                           # id da playlist do spotify
-#playlist_tracks = get_playlist_tracks(get_token(), playlist_id, playlist_youtube_id) # pega as musicas da playlist do spotify
-playlist_tracks = get_playlist_tracks(get_token(), playlist_id)                       # pega as musicas da playlist do spotify
+playlist_tracks = get_playlist_tracks(get_token(), playlist_id)                      # pega as musicas da playlist do spotify
+search_youtube_link(playlist_info)
+links = extract_links(playlist_info)
+save_links_to_env(links)
+"""
+
+#"""
+#send_links_to_youtube(youtube_links)
 #send_to_youtube(spotify_saved_playlist)
 #save_to_env(playlist_tracks)                                                         # salva as musicas da playlist do spotify no .env
 #has_new_music(playlist_tracks, playlist_id)                                          # verifica se há novas musicas na playlist do spotify
