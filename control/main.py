719c9cefe3a5274bd03e808203e48578976e8bcd
Vinícius <viniciushallmann2015@gmail.com>
2024-10-22 17:19:21 -0300

    Update: main restructured

--- control/main.py
+++ control/main.py
@@ -8,88 +8,60 @@ import argparse
 import os
 import json
 import base64
+
+from youtube import get_authenticated_service, build_youtube_service, search_youtube_link, create_playlist, add_song_youtube
+from spotify import get_token, get_playlist_tracks, get_playlist_name, get_playlist_description
 #Imports API GOOGLE
 playlist_info = []
 
 app = Flask(__name__)
-
+load_dotenv()
 @app.route('/')
 def index():
     return render_template('index.html')  # Carrega o index.html
 
 #LOAD DAS VARIAVEIS DO SPOTIFY NO .ENV
 def get_spotify_variables_env():
-    load_dotenv()
     return os.getenv("SPOTIFY_CLIENT_ID"), os.getenv("SPOTIFY_CLIENT_SECRET")
 
 #LOAD DAS VARIAVEIS DO YOUTUBE NO .ENV
 def get_youtube_variables_env():
-    load_dotenv()
     return os.getenv("YOUTUBE_CLIENT_ID"), os.getenv("YOUTUBE_CLIENT_SECRET"), os.getenv("YOUTUBE_TRACKS_LINKS")
 
 #========================================================================================================================================================#
-#YOUTUBE CONFIGS
-#"""
 
-#"""
 playlist_youtube_id = "PLJx7IIF4C47C-p_1epjTOwMm4jlmRzrEu"
 
 spotify_client_id, spotify_client_secret = get_spotify_variables_env()
 youtube_client_id, youtube_client_secret, youtube_links = get_youtube_variables_env()
 
-#SPOTIFY CONFIGS
-"""
-playlist_id = "3aJsKbVHL3U3ZngKqoVUZo?si=aa77820410b34402"                            # id da playlist do spotify
-playlist_tracks = get_playlist_tracks(get_token(), playlist_id)                      # pega as musicas da playlist do spotify
-search_youtube_link(playlist_info)
-links = extract_links(playlist_info)
-save_links_to_env(links)
-"""
-
-#"""
-#send_links_to_youtube(youtube_links)
-#send_to_youtube(spotify_saved_playlist)
-#save_to_env(playlist_tracks)                                                         # salva as musicas da playlist do spotify no .env
-#has_new_music(playlist_tracks, playlist_id)                                          # verifica se há novas musicas na playlist do spotify
-
-def return_playlist_id():
-    playlist_youtube_id = "PLJx7IIF4C47C-p_1epjTOwMm4jlmRzrEu"
-    return playlist_youtube_id
-
-#python3 /usr/local/scripts/main.py has_new_music {{workflow.variables['playlist_id']}} {{node['HTTP Request'].json['access_token']}}
+def get_playlist_id():
+    return "2A49ff4cDR5xLqzg8L511Q"
 
 def main():
-    """
-    credentials = get_authenticated_service()                     
-    youtube = build('youtube', 'v3', credentials=credentials) 
+    #WORKFLOW: -> RUN MAIN -> GET_BUILD -> GET_TOKEN -> GET_PLAYLIST_TRACKS -> GET_TRACKS_INFO 
+    #          -> GET_YOUTUBE_LINKS -> GET_YOUTUBE_ID -> GET_YOUTUBE_PLAYLIST -> GET_YOUTUBE_TRACKS 
+    #          -> GET_YOUTUBE_TRACKS_INFO -> ADD_YOUTUBE_TRACKS -> RETURN
+    #youtube = build_youtube_service()
+    spotify = get_token(spotify_client_id, spotify_client_secret)
 
-    spotify_token = get_token()
+    #GET SPOTIFY PLAYLIST
+    playlist_id_sp  = get_playlist_id()
+    playlist_info   = get_playlist_tracks(spotify, playlist_id_sp)
+    playlist_name   = get_playlist_name(spotify, playlist_id_sp)
+    playlist_desc   = get_playlist_description(spotify, playlist_id_sp)
 
-    parser = argparse.ArgumentParser(description='Run specific function from the script.')
-    parser.add_argument('function', type=str, help='The function to run')
-    parser.add_argument('playlist_id', type=str, nargs='?', default=None, help='Playlist ID')
-    parser.add_argument('token', type=str, help='OAuth2 Access Token')
-    
-    args = parser.parse_args()
+    print(playlist_id_sp)
+    print(playlist_info)
+    print(playlist_name)
+    print(playlist_desc)
 
-    youtube = build('youtube', 'v3', credentials=args.token) 
-    if args.function == 'return_playlist_id':
-        result = return_playlist_id()
+    #GET YOUTUBE LINKS
+    #youtube_playlist_id = create_playlist(youtube, playlist_name, playlist_desc)
+    #playlist_info       = search_youtube_link(playlist_info)
 
-    elif args.function == "create_playlist":
-        create_playlist(youtube, get_playlist_name(spotify_token, args.playlist_id), get_playlist_description(spotify_token, args.playlist_id))
+    pass
 
-    elif args.function == 'get_playlist_tracks':
-        get_playlist_tracks(spotify_token, return_playlist_id()) 
-        playlist_name = get_playlist_name(spotify_token, return_playlist_id())
-        print(f"Musicas resgatadas da playlist: '{playlist_name}'")
-
-    elif args.function == 'has_new_music':
-        result = has_new_music(args.playlist_id)
-        print(result)
-    else:
-        print(f"Function '{args.function}' not found")
-    """
 if __name__ == '__main__':
     main()
 
